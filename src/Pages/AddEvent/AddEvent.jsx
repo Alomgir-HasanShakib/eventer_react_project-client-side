3d80fa43cd76d50e1036a72f2635012314917f1e
Shakib <alomgirhasanshakib@gmail.com>
2024-05-11 14:12:22 +0600

    created add services form

--- src/Pages/AddEvent/AddEvent.jsx
+++ src/Pages/AddEvent/AddEvent.jsx
@@ -1,12 +1,102 @@
 import { Helmet } from "react-helmet-async";
 
 const AddEvent = () => {
+  const handleAddItem = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const imageUrl = form.photoUrl.value;
+    console.log(imageUrl);
+  };
   return (
-    <div>
+    <div className="mx-3">
       <Helmet>
         <title>Add Event</title>
       </Helmet>
-      AddEvent
+      <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800 mt-24 mb-16">
+        <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
+          Add Event Management Services
+        </h2>
+
+        <form onSubmit={handleAddItem}>
+          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="username"
+              >
+                Image URL
+                <input
+                  id="username"
+                  type="text"
+                  name="photoUrl"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </label>
+            </div>
+
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="emailAddress"
+              >
+                Service Name
+              </label>
+              <input
+                id="emailAddress"
+                name="serviceName"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="password"
+              >
+                Price
+              </label>
+              <input
+                id="password"
+                name="price"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="passwordConfirmation"
+              >
+                Service Area
+              </label>
+              <input
+                id="passwordConfirmation"
+                name="serviceArea"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label
+                className="text-gray-700 dark:text-gray-200"
+                htmlFor="passwordConfirmation"
+              >
+                Description
+              </label>
+              <textarea
+                id="passwordConfirmation"
+                name="description"
+                className="block w-full resize-none px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-end mt-6">
+            <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-primaryColor rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Add Service
+            </button>
+          </div>
+        </form>
+      </section>
     </div>
   );
 };
